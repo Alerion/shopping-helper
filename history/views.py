68b575fde44eff548516fd5992a1f43ac6a3b286
nezaets <olenamatsevko@gmail.com>
2013-08-15 15:43:52 +0300

    changed views and templates

--- history/views.py
+++ history/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
-
+from src.main.models import ShoppingList
 
 @login_required
 def index(request):
-    context = {}
+    sl = ShoppingList.objects.all()
+    arr=[]
+    for s in sl:
+	arr.append({'date':s.date,'products':s.products.all()})
+    context = {'shoppingList': arr}
     return TemplateResponse(request, 'history/index.html', context)
+  
+
+
