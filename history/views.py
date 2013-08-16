551f2afe1db015d380c641462393226583f62eb1
nezaets <olenamatsevko@gmail.com>
2013-08-16 15:05:19 +0300

    addedcategories menu and css file

--- history/views.py
+++ history/views.py
@@ -1,13 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
 from src.main.models import ShoppingList
-
+from src.main.models import Category
+from django.http import HttpResponse
 @login_required
 def index(request):
-    sl = ShoppingList.objects.all()
-    context = {'shoppingList': sl}
+    dash = request.user.getDashboard()
+    sl = dash.shoppinglist_set.all()
+    ct = Category.objects.all()
+    pr = dash.product_set.all()
+    context = {'shoppingList': sl, 'category': ct, 'products': pr}
     return TemplateResponse(request, 'history/index.html', context)
+
+
+
   
-
-
 
