d7ef7ddbf51e8b2b00c3c010d5ff21966e7c2bd0
volodiaKhol <me_prince@ukr.net>
2013-08-17 13:00:40 +0300

    added an example how to pull data from database, #3

--- src/main/views.py
+++ src/main/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
-
+from models import Dashboard
+from django.views.generic import ListView
 
 @login_required
 def index(request):
-    context = {}
+    s1=Dashboard.objects.all()
+    context = {'dashboardlist':s1}
     return TemplateResponse(request, 'main/index.html', context)
