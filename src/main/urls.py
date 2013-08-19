7e6fea7b4b62df870a4d13dafd3450bba4e024dc
ArsenBatyuchok <arsen.batyuchok@gmail.com>
2013-08-19 18:01:02 +0300

    deleted pagination, created a scroll on product's list. Created a view for ShoppingList model and inserted it in a template, working on that, #3

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('src.main.views',
     url(r'^$', 'index', name='index'),
+    url(r'^$', 'current_list', name='current_list'),
 )
--- src/main/views.py
+++ src/main/views.py
@@ -1,21 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
-from models import Dashboard, Product
+from models import Dashboard, Product, ShoppingList
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render_to_response
 from django.views.generic import ListView
 
 @login_required
 def index(request):
-    s1=Product.objects.all()
     list1=Product.objects.all()
-    paginator=Paginator(list1, 5, allow_empty_first_page=True)
-    page=request.GET.get("page")
-    try:
-        listproduct = paginator.page(page)
-    except PageNotAnInteger:
-        listproduct = paginator.page(1)
-    except EmptyPage:
-        listproduct = paginator.page(paginator.num_pages)
-    context = {'listproduct':listproduct}
+    context = {'listproduct':list1}
     return TemplateResponse(request, 'main/index.html', context)
+
+def current_list(request):
+    list2=ShoppingList.objects.all()
+    context = {'to_buy_product': list2}
+    return TemplateResponse(request, 'main/index.html', context)
+
