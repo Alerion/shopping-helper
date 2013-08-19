977fad87ecdfcbc8bc4f5a47c64e924390d840c3
volodiaKhol <me_prince@ukr.net>
2013-08-19 19:12:15 +0300

    finally info from two tables at one page, #3

--- src/main/urls.py
+++ src/main/urls.py
@@ -3,5 +3,4 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('src.main.views',
     url(r'^$', 'index', name='index'),
-    url(r'^current_list/$', 'current_list', name='current_list'),
 )
--- src/main/views.py
+++ src/main/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
-from models import Dashboard, Product, ShoppingList
+from models import Product, ShoppingList
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render_to_response
 from django.views.generic import ListView
@@ -8,13 +8,14 @@ from django.views.generic import ListView
 @login_required
 def index(request):
     list1=Product.objects.all()
-    context = {'listproduct':list1}
-    return TemplateResponse(request, 'main/index.html', context)
-
-
-def current_list(request):
     list2 = ShoppingList.objects.all()
-    context = {'shoppinglist': list2}
+    context = {'listproduct':list1 , 'shoppinglist':list2}
     return TemplateResponse(request, 'main/index.html', context)
+
+
+#def current_list(request):
+#    list2 = ShoppingList.objects.all()
+#    context = {'shoppinglist': list2}
+#    return TemplateResponse(request, 'main/index.html', context)
 
 
