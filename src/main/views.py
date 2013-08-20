997baa637710870ed9901f578216c59972ab1a87
volodiaKhol <me_prince@ukr.net>
2013-08-20 17:33:02 +0300

    Added showind admin's dashboard and meanwhile useless form to add element

--- src/main/views.py
+++ src/main/views.py
@@ -1,21 +1,36 @@
 from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
-from models import Product, ShoppingList
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from models import Product, ShoppingList, Dashboard
+from django import forms
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
-from django.views.generic import ListView
+from src.accounts.models import User
+from src.main.models import Category
 
 @login_required
 def index(request):
     list1=Product.objects.all()
-    list2 = ShoppingList.objects.all()
-    context = {'listproduct':list1 , 'shoppinglist':list2}
+    user=request.user # define who is logged in
+    currUserDashboard = Dashboard.objects.filter(users = user)
+    currBuyList = Product.objects.filter(dashboard = currUserDashboard)
+    context = {'listproduct':list1 , 'currUserDashboard':currUserDashboard, 'currBuyList': currBuyList,  'user':user}
     return TemplateResponse(request, 'main/index.html', context)
 
 
-#def current_list(request):
-#    list2 = ShoppingList.objects.all()
-#    context = {'shoppinglist': list2}
-#    return TemplateResponse(request, 'main/index.html', context)
+def contact(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = AddForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            prodName = form.cleaned_data['prodName']
+            prodCategory = form.cleaned_data['prodCategory']
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = AddForm() # An unbound form
+    return render_to_response('index.html', {'form': form,})
 
 
+class AddForm(forms.Form):
+    prodName=forms.CharField()
+    prodCategory=forms.CharField()
+    def test(self):
+        return self.prodName
