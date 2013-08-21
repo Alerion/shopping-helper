e69ba5aa687a20b390be9df0a31e30ecc8a6ebda
Mike <miha.000@gmail.com>
2013-08-21 17:04:38 +0400

    changed IDs in template changed update_timeline in views #3

--- src/static/jquery/history.js
+++ src/static/jquery/history.js
@@ -1,6 +1,9 @@
 /*jQuery time*/
+//loop trought shopping list
+
 $(document).ready(function(){
-	$("#accordian h3").click(function(){
+
+$("#accordian h3").click(function(){
 		//slide up all the link lists
 		$("#accordian ul ul").slideUp();
 		//slide down the link list below the h3 clicked - only if its closed
@@ -13,31 +16,21 @@ $(document).ready(function(){
 	mass=$("#acordionForm input[type='checkbox']:checked")
 
 
-
-
-
-
-
-
-
-
-
-
  $("#list_button").click(function() {
 
-    var url = "http://127.0.0.1:8000/history/update_timeline"; // the script where you handle the form input.
+    var url = "/history/update_timeline";
     var data=$("#acordionForm").serialize();
     $.ajax({
            type: "POST",
            url: url,
-           data: data, // serializes the form's elements.
+           data: data, 
            success: function(data)
            {
-               alert(data); // show response from the php script.
+               alert(data); 
            },
         
          });
 
-    return false; // avoid to execute the actual submit of the form.
+    return false; 
 });
 })
