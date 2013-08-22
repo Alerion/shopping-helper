e02b1a040c4d15e5a95b371cf1c4240709d97956
nezaets <olenamatsevko@gmail.com>
2013-08-22 20:05:07 +0300

    added url for work with ajax

--- history/urls.py
+++ history/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('history.views',
     url(r'^$', 'index', name='index'),
 	url(r'^update_timeline$', 'update_timeline', name='update_timeline'),
+	url(r'^information$', 'information', name='information'),
 )
 
