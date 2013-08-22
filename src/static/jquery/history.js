a2639084e967b495379983758fbc372f4ace5db5
nezaets <olenamatsevko@gmail.com>
2013-08-22 22:19:00 +0300

    fix problem with products appearance (when  container was hidden)

--- src/static/jquery/history.js
+++ src/static/jquery/history.js
@@ -2,7 +2,6 @@
 //loop trought shopping list
 
 $(document).ready(function(){
-
     $("#accordian h3").click(function(){
     		//slide up all the link lists
     		$("#accordian ul ul").slideUp();
@@ -13,20 +12,7 @@ $(document).ready(function(){
     		}
     	})
 
-
-    function hideEmpty(){
-
-        var slDivs = $(".shopping-list"); 
-        for (var i = 0; i < slDivs.length; i++) {
-            var sl_products = $("#"+slDivs[i].id).find(".product:visible");
-            if (sl_products.length == 0){
-            $("#"+slDivs[i].id).hide();
-            }
-        }
-    }
-
     $('.category').change(function(){
-
         var slDivs = $(".shopping-list");
         var products = $("#li_category"+this.id).find(".products")
         var prodId = []
@@ -38,43 +24,57 @@ $(document).ready(function(){
             prodId.push(id.slice(n+1))
         }
         
-        if ($(this).is(':checked')) {
+        if ($(this).is(':checked') ) {
             products.prop("checked", true);/*cant get id direct, need use prop. Why ?*/
             for (var j = 0; j< prodId.length; j++) {
             $('.product_' + prodId[j]).show();
+            $('.product_' + prodId[j]).parents('.shopping-list').show();
             }
-        } else { products.prop("checked", false);
-            for (var j = 0; j< prodId.length; j++){
-            $('.product_' + prodId[j]).hide();
-        } 
-        hideEmpty();
+        }
+        else {products.prop("checked", false);
+            for (var j = 0; j< prodId.length; j++) {
+                $('.product_' + prodId[j]).hide();
+            }
+        hide_Block()
         }
     })
-    
+
+    function hide_Block(){
+        var slDivs = $(".shopping-list"); 
+        for (var i=0; i<slDivs.length; i++){
+            var sl_products = $("#"+slDivs[i].id).find(".product:visible");
+            if (sl_products.length == 0){ 
+                $("#"+slDivs[i].id).hide();
+            }
+        }
+    }
 
     $('.products').change(function(){
         var id = $(this).attr('id')
         var n = id.indexOf('_')
         var categoryId = id.slice(0,n)
         var p = $('.products')
+        var slDivs = $(".shopping-list");  
 
         //TODO:for some reason products in checkbox menu and timeline have different IDs
         var productId =id.slice(n+1);
 
         //id of product list item
         var liId = '.product_' + productId; 
-        //var slDivs = $(".shopping-list"); 
         if ($(this).is(':checked')) {
             $('#'+categoryId).prop("checked", true);//check outer checkbox
-            $(liId).show();
+            $(liId).show(); 
+            $(liId).parents('.shopping-list').show()
         } else {
             //hide unchecked product
             $(liId).hide();
-            hideEmpty();
-            //when all bolock uncheked
+            //hide empty block with shopping-list
+            hide_Block();
+              //when all bolock uncheked
             if ($('#ul_products_'+categoryId).find("input:checkbox:checked").length == 0 ) {
                 $('#'+categoryId).prop("checked", false);
-            }    
+            }
+            
         }
     })
 
@@ -92,7 +92,7 @@ $(document).ready(function(){
                    alert(data); 
                },
             
-        });
+             });
 
         return false; //???
     });
