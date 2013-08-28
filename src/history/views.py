6217f1cf2b2cf95f96d0da69b4912c3b94ce3335
Dmitriy Kostochko <alerion.um@gmail.com>
2013-08-28 23:49:00 +0300

    fix history app

--- src/history/views.py
+++ src/history/views.py
@@ -6,21 +6,23 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.http import HttpRequest
 import simplejson
+
+
 @login_required
 def index(request):
     dash = request.user.get_dashboard()
-    
+
     categoriesAll = Category.objects.all() #getting queryset all categories
     categoriesProduct = []
-    #sizes of shopping list circles css 
+    #sizes of shopping list circles css
     sizeTemplate = range(2,22,2)
-    sizeTemplate.insert(0, None)    
+    sizeTemplate.insert(0, None)
 
     for category in categoriesAll:
         products = dash.product_set.filter(category__id=(category.id))
         categoriesProduct.append({"products":products,"category":category})
 
-      
+
     lastDate = None
     sizeOfCircle  = None
     shoppingLists = []
@@ -42,12 +44,16 @@ def index(request):
 
         shoppingLists.append({"sList": sList,"Size":sizeOfCircle,"Distance":distanceDays*50});
 
-    context = {'category':categoriesAll, 'categoriesProduct':categoriesProduct, 'shoppingList' :shoppingLists}
+    context = {
+        'category':categoriesAll,
+        'categoriesProduct':categoriesProduct,
+        'shoppingList' :shoppingLists
+    }
     return TemplateResponse(request, 'history/index.html', context)
 
 
 def update_timeline(request):
-    
+
     response = request.POST
     '''????'''
 
