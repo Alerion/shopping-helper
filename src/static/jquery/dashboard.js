1d69cd6b6934491d730709d3c25e5c32353f2596
ArsenBatyuchok <arsen.batyuchok@gmail.com>
2013-09-10 17:54:00 +0300

    added images with category icons to AJAX response, #3

--- src/static/jquery/dashboard.js
+++ src/static/jquery/dashboard.js
@@ -19,10 +19,18 @@ jQuery(function($) {
         $('.choose-item').on('click',function(){
             var $this = $(this);
             var product_id = $this.data('product-id');
+            var product_icon = $this.data('item-icon');
+
             $.post(URLS.ADD_ITEM,{'product_id':product_id},function(response){
+
                 $(".items_of_buylist").prepend(
-                    '<p class="product-item">'+
-                    response+
+                    '<p class="product-item" data-item-icon="' +
+                     product_icon +
+                     '">' +
+                     '<img src="' + product_icon + '" />' +
+                    '<span class="pdf">' +
+                    response +
+                     '</span>' +
                     '<i data-product-id="'+
                     product_id +
                     '" class="icon-remove-circle remove-product"></i></p>');
