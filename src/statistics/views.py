e34f1faad6614831d24b74c691ccc04288ba9287
Vasyl Kotsovskyi <kotsovskyi@gmail.com>
2013-09-23 07:55:00 +0300

    separate template and js, make ajax query

--- src/statistics/views.py
+++ src/statistics/views.py
@@ -2,12 +2,18 @@ import json
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 from django.template import RequestContext, loader
 from src.main.models import Product, Category, ShoppingList
 
 
 @login_required
 def index(request):
+
+    return TemplateResponse(request, 'statistics/index.html')
+
+@login_required
+def back_page(request):
     user = request.user
     dashboard  =  request.user.get_dashboard()
     categories  =  Category.objects.all()
@@ -22,21 +28,20 @@ def index(request):
         categories_name.append(i.name)
     price_by_month = get_price_by_date()#generate data for time chart
     cat_price=[]
-    for h in categories:
+    for h in categories:                #generate data for stack area
         data_for_stacked_area.append({
             'data' : get_month_price_by_category(h.pk),
             'name' : h.name
         })
-    template  =  loader.get_template('statistics/index.html')
-    context  =  RequestContext(request, {
+    mimetype = 'application/json'
+    context  =  {
         'data_for_piechart': json.dumps(data),
         'price' : json.dumps(price),
         'categories_name' : json.dumps(categories_name),
-        'price_by_month' : price_by_month,
+        'price_by_month' : json.dumps(price_by_month),
         'data_for_stacked_area' : json.dumps(data_for_stacked_area)
-    })
-    return HttpResponse(template.render(context))
-
+    }
+    return HttpResponse(json.dumps(context),mimetype)
 '''
 @param -- category's id
 get_category_price() --return percentage of spended money for each category----
@@ -60,6 +65,7 @@ def get_price_by_date():
         price_by_month.append(int(price))
     return price_by_month
 '''
+@param -- category's id
 get_month_price_by_category(id) -- return summ manthly spended
 for each category
 '''
