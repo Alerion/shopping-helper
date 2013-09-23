14de4e3415d5c447016691aa62939e111a1dc8ce
Vasyl Kotsovskyi <kotsovskyi@gmail.com>
2013-09-23 11:51:24 +0300

    merge from master

--- src/utils/views.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from django.http import HttpResponse
-
-
-class JSONResponse(HttpResponse):
-    def __init__(self, data):
-        output = json.dumps(data)
-        super(JSONResponse, self).__init__(output, mimetype="application/json")
