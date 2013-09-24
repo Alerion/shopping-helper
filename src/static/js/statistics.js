571d8f94af2847f734f590ef361187dcae358315
Vasyl Kotsovskyi <kotsovskyi@gmail.com>
2013-09-24 23:49:24 +0300

    New librery, move on backbone

--- src/static/js/statistics.js
+++ src/static/js/statistics.js
@@ -3,184 +3,96 @@
                     window.a = {};
                     $.ajax({
                         url : 'http://127.0.0.1:8000/statistics/back_page/',
-			dataType : 'json',
+                        dataType : 'json',
                         async : false,
                         success : function(data) {
                             window.a = data;
                         }
                     });
                 })
-    $(function () {
-    var chart;
-    
-    $(document).ready(function () {
-        $('#chart1').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
+var PieCartView = Backbone.View.extend({
+    el: "#chart1",
+    initialize : function(){
+        this.render();
+    },
+    render : function(){
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable(JSON.parse(a.data_for_piechart));
 
-            },
-            title: {
-                text: 'Spends by category'
-            },
-            tooltip: {
-        	    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                type: 'pie',
-                name: 'Procentege',
-                data: JSON.parse(a.data_for_piechart)
-            }]
-        });
-    });
+        var options = {
+          title: 'Money spended for each category'
+        };
 
+        var chart = new google.visualization.PieChart(document.getElementById('chart1'));
+        chart.draw(data, options);
+      }
+    }
 });
-    /*Bar chart*/
-$(function () {
-        $('#bar_chart').highcharts({
-            chart: {
-                type: 'column',
-                margin: [ 50, 50, 100, 80]
-            },
-            title: {
-                text: 'Money spend for each category'
-            },
-            xAxis: {
-                categories: JSON.parse(a.categories_name),
-                labels: {
-                    rotation: -45,
-                    align: 'right',
-                    style: {
-                        fontSize: '13px',
-                        fontFamily: 'Verdana, sans-serif'
-                    }
-                }
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'price '
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            tooltip: {
-                pointFormat: 'Price {point.y:.1f}',
-            },
-            series: [{
-                name: 'Price',
-                data: JSON.parse(a.price),
-                dataLabels: {
-                    enabled: true,
-                    color: '#999',
-                    align: 'center',
-                    style: {
-                        fontSize: '13px',
-                        fontFamily: 'Verdana, sans-serif',
-                        textShadow: '0 0 3px #cecece'
-                    }
-                }
-            }]
-        });
-        
-    });
+var BarChartView = Backbone.View.extend({
+    el: "#bar_chart",
+    initialize : function(){
+        this.render();
+    },
+    render : function(){
+        google.load("visualization", "1", {packages:["corechart"]});
+        google.setOnLoadCallback(drawChart);
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable(JSON.parse(a.data_for_bar_chart));
+            var options = {
+                title: 'Money spended for each category',
+                hAxis: {title: 'categories', titleTextStyle: {color: 'red'}},
 
-    $(function () {
-        $('#time_series').highcharts({
-            chart: {
-                type: 'spline'
-            },
-            title: {
-                text: 'Monthly Spends'
-            },
-            
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            },
-            yAxis: {
-                title: {
-                    text: 'price'
-                },
-                labels: {
-                    formatter: function() {
-                        return this.value
-                    }
-                }
-            },
-            tooltip: {
-                crosshairs: true,
-                shared: true
-            },
-            plotOptions: {
-                spline: {
-                    marker: {
-                        radius: 4,
-                        lineColor: '#666666',
-                        lineWidth: 1
-                    }
-                }
-            },
-            series: [{
-                name: 'price',
-                data: JSON.parse(a.price_by_month)
-            }]
-        });
-    });
-    
-    $(function () {
-        $('#stacked_area').highcharts({
-            chart: {
-                type: 'area'
-            },
-            title: {
-                text: 'Monthly spends by categories'
-            },
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-                tickmarkPlacement: 'on',
-                title: {
-                    enabled: false
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'spends'
-                },
-                labels: {
-                    formatter: function() {
-                        return this.value;
-                    }
-                }
-            },
-            tooltip: {
-                shared: true,
-                valueSuffix: ' '
-            },
-            plotOptions: {
-                area: {
-                    stacking: 'normal',
-                    lineColor: '#666666',
-                    lineWidth: 1,
-                    marker: {
-                        lineWidth: 1,
-                        lineColor: '#666666'
-                    }
-                }
-            },
-            series: JSON.parse(a.data_for_stacked_area)
-        });
-    });
+            };
+            var chart = new google.visualization.ColumnChart(document.getElementById('bar_chart'));
+            chart.draw(data, options);
+      }
+    }
+});
+var TimeSeriesView = Backbone.View.extend({
+    el : "#time_series",
+    initialize : function(){
+        this.render();
+    },
+    render : function(){
+        google.load("visualization", "1", {packages:["corechart"]});
+        google.setOnLoadCallback(drawChart);
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable(JSON.parse(a.price_by_month));
+
+        var options = {
+          title: 'Company Performance'
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('time_series'));
+        chart.draw(data, options);
+      }
+    }
+});
+var StackedAreaView = Backbone.View.extend({
+    el : '#stacked_area',
+    initialize : function(){
+        this.render();
+    },
+    render : function(){
+        google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable(JSON.parse(a.data_for_stacked_area));
+
+        var options = {
+          title: 'Company Performance',
+          hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+          vAxis: {minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('stacked_area'));
+        chart.draw(data, options);
+      }
+    }
+});
+var pieChartView = new PieCartView();
+var barChartView = new BarChartView();
+var timeSeriesView = new TimeSeriesView();
+var stackedAreaView = new StackedAreaView();
--- src/statistics/views.py
+++ src/statistics/views.py
@@ -17,29 +17,24 @@ def back_page(request):
     user = request.user
     dashboard  =  request.user.get_dashboard()
     categories  =  Category.objects.all()
-    data = []
-    data_for_stacked_area=[]
-    for obj in categories:              #generate data for piechart
+    data = [['category name', 'category price']]
+    for obj in categories:                           #generate data for piechart
         data.append([ obj.name, get_category_price(obj.pk) ]) 
-    price  =  []
-    categories_name = []
-    for i in categories:                #generate data for barchart
-        price.append(get_category_price(i.pk))
-        categories_name.append(i.name)
-    price_by_month = get_price_by_date()#generate data for time chart
-    cat_price=[]
-    for h in categories:                #generate data for stack area
-        data_for_stacked_area.append({
-            'data' : get_month_price_by_category(h.pk),
-            'name' : h.name
-        })
+    data_for_bar_chart = [['category name', '']]
+    for i in categories: 
+        data_for_bar_chart.append([i.name, get_category_price(i.pk)])   #generate data for barchart
+    price_by_month = get_price_by_date()             #generate data for time chart
+    data_for_stacked_area=[['month','Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']]
+    for h in categories:
+        a = get_month_price_by_category(h.pk)
+        a.insert(0, h.name)
+        data_for_stacked_area.append(a)
     mimetype = 'application/json'
     context  =  {
-        'data_for_piechart': json.dumps(data),
-        'price' : json.dumps(price),
-        'categories_name' : json.dumps(categories_name),
+        'data_for_piechart' : json.dumps(data),
+        'data_for_bar_chart' : json.dumps(data_for_bar_chart),
         'price_by_month' : json.dumps(price_by_month),
-        'data_for_stacked_area' : json.dumps(data_for_stacked_area)
+        'data_for_stacked_area' : json.dumps(zip(*data_for_stacked_area))
     }
     return HttpResponse(json.dumps(context),mimetype)
 '''
@@ -56,13 +51,15 @@ def get_category_price(id):
 get_price_by_date() -- function to take data fir time line grafic
 '''
 def get_price_by_date():
-    price_by_month = []
+    price_by_month = [['month', '']]
     products = Product.objects.all()
+    monthes = ['','Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+               'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     for month in range(13)[1:]:
         price = 0
         for obj in products.filter(shoppinglist__date__month = month):
             price += obj.price
-        price_by_month.append(int(price))
+        price_by_month.append([monthes[month], int(price)])
     return price_by_month
 '''
 @param -- category's id
