1ecf3b01404d787513ad182ef06d9f94942f41f0
Mike <miha.000@gmail.com>
2013-09-27 20:26:40 +0300

    fixed bug with firefox NotSupportedError. Menu stops floating when bigger then window

--- src/static/js/history/helper.js
+++ src/static/js/history/helper.js
@@ -1,4 +1,3 @@
-//$.Helper = $.Helper || {};
 
     var localProducts = new $.Helper.Products();
 
@@ -15,26 +14,87 @@
     var shoppingLists = new $.Helper.TimeLine();
     var currentProducts = new $.Helper.Products();
 
-    shoppingLists.fetch({ async:false,
-
+    // async:false for some reason gives errors in firefox. App crushes
+    //NotSupportedError: Operation is not supported
+    shoppingLists.fetch({
+            
                 success : function() {
                    
                     var currentList = shoppingLists.where({ 'date': null})[0];
                     var products = currentList.get('products');
+                    
                     _.each(products.models, function(product){
                         currentProducts.add(product)
                     })
                     
                 }
-            })
+                
+    })
+    
     $.Helper.timer = "";
 
     $.fn.center = function () {
-    this.css("position","absolute");
-    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
-                                                $(window).scrollTop()) + "px");
-    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
-                                                $(window).scrollLeft()) + "px");
-    return this;
+        this.css("position","absolute");
+        this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                    $(window).scrollTop()) + "px");
+        this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                    $(window).scrollLeft()) + "px");
+        return this;
     }
-    
+
+    //menu floating
+    $(function(){
+
+        var docHeight = $(document).height();
+
+        function moveFloatMenu() {
+         
+            //top position of accordian addad scrollTop position of window
+          
+            var menuOffset = menuYloc.top + $(this).scrollTop();
+            var accordianHeight = parseInt($('#accordian').css('height'),10);
+            var floatMenu = true;
+            var accordianPosition = parseInt($('#accordian').offset().top,10);
+
+            //to determine if accordion is visible
+            var OffsetDifference = Math.abs(accordianPosition - menuOffset);
+
+            if(!this.docHeight){
+        
+                this.docHeight = $(document).height();
+            }
+      
+            if((parseInt(menuOffset,10) + accordianHeight) > this.docHeight){
+
+                floatMenu = false;
+            }
+            else if($(window).height() < accordianHeight && OffsetDifference < $(window).height()/2){
+
+                 floatMenu = false;
+                
+            }
+     
+            if (floatMenu){
+                $('#accordian').animate({
+                    top: menuOffset+"px"
+                }, {
+                    duration: 1000,
+                    queue: false
+                });
+            }
+       
+        
+        }
+        //returns the offset coordinates for the selected elements, relative to the document.
+       var menuYloc = $('#accordian').offset();
+        //The scroll event occurs when the user scrolls in the specified element
+        //the window is scrolled, moveFloatMenu works
+
+        $(window).scroll(function(){
+            var menuHeight = ($('#accordian').css('height'))
+            moveFloatMenu();
+          
+
+        });  
+});
+   
