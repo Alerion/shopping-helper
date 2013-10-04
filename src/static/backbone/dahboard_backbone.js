f9c4006783c6c405caeb7c8e47b52d4a1af233e0
Volodia <volodia@ubuntu.(none)>
2013-10-04 11:19:30 +0300

    ...

--- src/static/backbone/dahboard_backbone.js
+++ src/static/backbone/dahboard_backbone.js
@@ -251,6 +251,21 @@
                 $('#home').hide();
              }
         });
+        $(document).ready(function(){
+            $('section[data-type="background"]').each(function(){
+                var $bgobj = $(this); // assigning the object
+             
+                $(window).scroll(function() {
+                    var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
+                     
+                    // Put together our final background position
+                    var coords = '50% '+ yPos + 'px';
+         
+                    // Move the background
+                    $bgobj.css({ backgroundPosition: coords });
+                });
+            });   
+        });
         //Creating objects to work with
         $(".b-popup-content").hide();
         $('.b-popup').hide();
