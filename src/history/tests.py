aaaafd905e15a3f0073cb5c292f575b8a366cff6
nezaets <olenamatsevko@gmail.com>
2013-10-23 19:53:27 +0300

    added new tests

--- src/history/tests.py
+++ src/history/tests.py
@@ -29,23 +29,9 @@ class ViewErrorTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.c = Client()
-        #self.c.login(username='admin', password='admin')
         self.c.post('/login/', {'username': 'admin', 'password': 'admin'})
 
-    def testView(self):
-        response = self.c.post('/login/', {'username': 'admin', 'password': 'admin'})
-        print response.status_code
-        #response = self.c.get('/history/prices/')
-        #print response.content
-        #response = self.c.post('/history/old/')
-        #print response.content
-        #response = self.c.get('/history/work_with_map/', {'id': 1})
-        #print response
-        #print self.c.get('/history/work_with_map/?id =1')
-        #self.assertEqual(self.c.get('/history/work_with_map/', {'id': 1}).loc_names, ["Home Shop", "Magnus", "Arsen", "Colobock"])
-
     def test_prices(self):
-
         request = self.factory.get('/history/prices/')
         response = views.prices(request)
         self.assertEqual(response.status_code, 200)
@@ -71,22 +57,15 @@ class ViewErrorTest(TestCase):
         response = self.c.get('/history/information/',{'id':1})
         self.assertEqual(response.status_code, 200)
 
-    def test_add_to_list_id(self):
+    def test_list_id_correct(self):
         response = self.c.get('/history/add_to_list/',{'id':1})
         self.assertEqual(response.status_code, 200)
 
-    def test_add_to_list_id(self):
+    def test_list_id_incorrect(self):
         response = self.c.get('/history/add_to_list/',{'id':-1})
         self.assertEqual(response.status_code, 200)
 
-    def test_add_to_list_(self):
-        
-        """dash = request.user.get_dashboard() 
-        product = dash.product_set.filter(id=(product_id))[0]
-        curr_buylist = dash.get_or_create_shopping_list()
-        products_in = curr_buylist.products.all()
-        product_ids = []
-        """
+    def test_add_to_list_correct(self):
         response = self.c.get('/history/add_to_list/',{'id':1})
         self.assertEqual(response.status_code, 200)
             
@@ -103,11 +82,11 @@ class ViewErrorTest(TestCase):
         response = self.c.get('/history/work_with_map/')
         self.assertEqual(response.status_code, 200)
 
+    def test_map_id_correct(self):
+        response = self.c.get('/history/work_with_map/',{'id':1})
+        self.assertEqual(response.status_code, 200)
 
-#class TestMainViews(TestCase):
-    #urls = 'history'
-    #def setUp(self):
-        #self.c = Client()
-    #def testIndexPageView(self):
-        # Here you'd test your view using ``Client``.
-        #call_some_test_code()
+    def test_map_id_incorrect(self):
+        response = self.c.get('/history/work_with_map/',{'id':-1})
+        self.assertEqual(response.status_code, 200)
+
